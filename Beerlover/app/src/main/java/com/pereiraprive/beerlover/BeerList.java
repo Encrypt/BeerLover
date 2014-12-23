259cab1666316b91812f0c7e6451bceb03921acc
Yann Privé <prive@et.esiea.fr>
2014-12-23 15:42:54 +0100

    Passage sur le nouvel Android Studio

---
+++ Beerlover/app/src/main/java/com/pereiraprive/beerlover/BeerList.java
@@ -0,0 +1,13 @@
+package com.pereiraprive.beerlover;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BeerList extends Activity {
+
+    protected void onCreate() {
+
+        // Set the "beer_list" content
+        setContentView(R.layout.beer_list);
+    }
+}
