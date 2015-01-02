7bba1d16615436cb86985ddc41bafac926456d24
Yann Privé <prive@et.esiea.fr>
2015-01-02 13:14:10 +0100

    Avancement de la BeerList

--- Beerlover/app/src/main/java/com/pereiraprive/beerlover/BeerList.java
+++ Beerlover/app/src/main/java/com/pereiraprive/beerlover/BeerList.java
@@ -6,25 +6,48 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ExpandableListView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
 public class BeerList extends ActionBarActivity {
 
-    // Needed variables for this view
+    // Needed objects for this view
     private String currentSort = "Type";
+    private ArrayList<Integer> beerID, beerTypeID, beerOriginID;
+    private ArrayList<String> beerName;
     private ExpandableListView expandableListView;
 
     // Method called once the Activity is launched
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+
+        // Immediately starts downloading the complete JSON
+        downloadAllBeers();
+
         // Set the "beer_list" content
         setContentView(R.layout.beer_list);
+
+        // Fill the ExpendableView
+        fillView();
+
+    }
+
+    // Method to fill the expendable View
+    private void fillView() {
+
+        // Local objects
+        JSONObject jsonCategories = null;
+
         // Retrieves the ExpendableListView view
         expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
+
+        // Retrieves the categories
+        jsonCategories = DownloadCategory();
+
 
     }
 
@@ -50,26 +73,73 @@ public class BeerList extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public JSONObject DownloadJsonType (String type){
+    // Downloads the complete JSON and puts it in the variable
+    private void downloadAllBeers() {
 
+        // Local objects
+        DownloadTask dlTask;
+        JSONArray jsonArray;
+        JSONObject jsonObject, jsonOrigin;
+        String jsonString = null;
 
-        DownloadTask dltask = new DownloadTask();
-        JSONObject categorie_json=null;
-        String cat=null;
+        // Creates a new download task with the appropriate random value
+        dlTask = new DownloadTask();
 
-        // download the files to fill the Expandable listview
-        dltask.execute("GET", "http://binouze.fabrigli.fr/" +type+ ".json" ,"text");
+        // Retrieves all the beers
+        dlTask.execute("GET", "http://binouze.fabrigli.fr/bieres/bieres.json", "TXT");
+
+        try {
+            jsonString = dlTask.get();
+        }
+        catch(InterruptedException | ExecutionException e) {}
+
+        // Parse the beers
         try {
 
-            cat = dltask.get();
+            jsonArray = new JSONArray(jsonString);
+
+            for(int i = 0 ; i < jsonArray.length() ; i++) {
+
+                // Takes the next JSON Object
+                jsonObject = jsonArray.getJSONObject(i);
+
+                // Retrieves the name, category and ID of the beer to put it in the ArrayLists
+                beerID.add(jsonObject.getInt("biere_id"));
+                beerTypeID.add(jsonObject.getInt("category_id"));
+                beerName.add(jsonObject.getString("name"));
+
+                // Retrieves the ID of the origin
+                jsonOrigin = jsonObject.getJSONObject("country");
+                beerOriginID.add(jsonOrigin.getInt("id"));
+
+            }
+        }
+
+        catch(JSONException e) {}
+
+    }
+
+    // Method to download the JSON associated to the category
+    private JSONObject DownloadCategory() {
+
+        DownloadTask dlTask = new DownloadTask();
+        JSONObject jsonCategory = null;
+        String webContent = null;
+
+        // Downloads the beers to fill the Expandable Listview
+        dlTask.execute("GET", "http://binouze.fabrigli.fr/" + currentSort + ".json" ,"text");
+
+        try {
+
+            webContent = dlTask.get();
         }
         catch(InterruptedException | ExecutionException e) {}
 
         try{
-            categorie_json = new JSONObject(cat);
+            jsonCategory = new JSONObject(webContent);
         }
         catch (JSONException e){}
 
-        return categorie_json;
+        return jsonCategory;
     }
 }
